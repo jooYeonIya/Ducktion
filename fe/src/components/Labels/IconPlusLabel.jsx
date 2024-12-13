c3b315fd3b564258cfac5cdf184d311bb23c958d
jooyeon <jooyeonstudyios@gmail.com>
2024-12-13 15:15:15 +0900

    design: IconPlusLabel 추가 (#1)

---
+++ fe/src/components/Labels/IconPlusLabel.jsx
@@ -0,0 +1,16 @@
+import '@styles/Labels.css';
+
+export default function({icon, text, onClick}) {
+  return (
+    <div className='iconPlusLabel' onClick={onClick}>
+      <img 
+        className='iconPlusLabel_icon'
+        src={icon} 
+        alt={text} 
+        width={40}
+        height={40}
+      />
+      <span className='iconPlusLabel_text'>{text}</span>
+    </div>
+  )
+}
