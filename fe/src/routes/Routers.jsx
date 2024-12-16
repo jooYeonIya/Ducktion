5905d5a03b650640ff3bfa53058fffa273c12690
bbman9900 <tmddn1679@naver.com>
2024-12-16 20:05:20 +0900

    feat: 커뮤니티 목록 보기 페이지 구현 (#3)

--- fe/src/routes/Routers.jsx
+++ fe/src/routes/Routers.jsx
@@ -1,10 +1,12 @@
 import {Routes, Route } from 'react-router-dom'
 import RequestAddCommunity from '../pages/RequestAddCommunity'
+import ViewCommunityList from '../pages/ViewCommunityList';
 
 export default function Routers() {
   return (
     <Routes>
       <Route path='/requestAddCommunity' element={<RequestAddCommunity />} />
+      <Route path="/viewCommunityList" element={<ViewCommunityList />} />
     </Routes>
   );
 }
