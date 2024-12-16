f149c2a66b5a2dc1d2c0a3e08a2846ff56daf1e3
jooyeon <jooyeonstudyios@gmail.com>
2024-12-16 19:24:00 +0900

    design: 마감 임박 상품, 마스터즈컬렉셔즈레어 상품 카드 목록 추가 (#13)

--- fe/src/pages/Home.jsx
+++ fe/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react'
-import { getPopularCategories } from '../services/itemsService'
+import { getPopularCategories, getClosingSoonItems, getMastersCollectorsRare } from '../services/itemsService'
 import IconPlusLabel from '../components/Labels/IconPlusLabel'
 import GodoTitleLabel from '../components/Labels/GodoTitleLabel'
 import '@styles/pages/Home.css'
+import ItemCard from '../components/ItemCard/ItemCard'
 
 export default function Home() {
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState([]);
+  const [closingSoonItems, setClosingSoonItems] = useState([]);
+  const [mastersItems, setMastersItems] = useState([]);
 
   const fetchPopularCategories = async () => {
     try {
@@ -16,8 +19,28 @@ export default function Home() {
     }
   };
 
+  const fetchClosingSoonItems = async () => {
+    try {
+      const data = await getClosingSoonItems();
+      setClosingSoonItems(data);
+    } catch (error) {
+      console.error("Faild", error);
+    }
+  }
+
+  const fetchMastersItems = async () => {
+    try {
+      const data = await getMastersCollectorsRare();
+      setMastersItems(data);
+    } catch (error) {
+      console.error("Faild", error);
+    }
+  }
+
   useEffect(() => {
     fetchPopularCategories();
+    fetchClosingSoonItems();    
+    fetchMastersItems();
   }, [])
 
   return (
@@ -32,6 +55,28 @@ export default function Home() {
           {categories && categories.map((item) => (
             <div className='home_popularCategories_item'>
               <IconPlusLabel text={item.title} icon={item.isFavorited ? "/src/assets/duck_selected.png" : "/src/assets/duck.png"} />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='home_cardItems'>
+        <GodoTitleLabel text={"마감 임박 상품"} />
+        <div className='home_cardItems_container'>
+          {closingSoonItems && closingSoonItems.map((item) => (
+            <div className='home_cardItems_item'>
+              <ItemCard data={item} />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='home_cardItems'>
+        <GodoTitleLabel text={"마스터즈컬렉션즈레어 상품"} />
+        <div className='home_cardItems_container'>
+          {mastersItems && mastersItems.map((item) => (
+            <div className='home_cardItems_item'>
+              <ItemCard data={item} />
             </div>
           ))}
         </div>
--- fe/src/services/itemsService.js
+++ fe/src/services/itemsService.js
@@ -14,7 +14,54 @@ export async function getPopularCategories() {
     }
     return data;
   } catch (error) {
-    console.error("Error fetching rooms:", error);
+    console.error("Error fetching:", error);
     throw error; 
   }
 }
+
+// 홈 페이지 - 마감 임박 상품
+export async function getClosingSoonItems() {
+  try {
+    // const response = await axios.get("http://localhost:8080/api/items/closingsoon");
+    // return response.data;
+    const data = [];
+    for (let i = 0; i < 5; i++) {
+      data.push({
+        image: "/src/assets/test_image.png",
+        favorited: i%2 == 0,
+        name: "으아아아악!!!!!! 리액트 고수 어디 없나!!!!!!",
+        priceInfo: {price: i*10000, type: ""},
+        additionalInfo: null,
+        overlayText: false
+      });
+    }
+    return data;
+  } catch (error) {
+    console.error("Error fetching:", error);
+    throw error; 
+  }
+}
+
+// 홈 페이지 - 마스터즈컬렉션즈레어 상품
+export async function getMastersCollectorsRare() {
+  try {
+    // const response = await axios.get("http://localhost:8080/api/items/masterscollectorsrare");
+    // return response.data;
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+      data.push({
+        image: "/src/assets/test_image.png",
+        favorited: i%2 == 0,
+        name: "이거슨 마스터 컬렉션즈 레어 상품이다 이놈들아",
+        priceInfo: {price: i*10000, type: ""},
+        additionalInfo: null,
+        overlayText: false
+      });
+    }
+    return data;
+  } catch (error) {
+    console.error("Error fetching:", error);
+    throw error; 
+  }
+}
+
