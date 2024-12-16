28adcf1973994f248932703c780c6fbc41e54bb2
glennman1 <uuusop@naver.com>
2024-12-16 17:46:51 +0900

    feat: 커뮤니티 개설 요청 페이지 불필요한 props 삭제 OnSubmit만 남겨둠 (#9)

--- fe/src/pages/RequestAddCommunity.jsx
+++ fe/src/pages/RequestAddCommunity.jsx
@@ -1,62 +1,19 @@
-import React, { useState } from 'react';
-import PostForm from '../components/PostForm'; //css 임포트
-
+import React from 'react';
+import PostForm from '../components/PostForm'; // css 임포트
 
 const RequestAddCommunity = ({ onSubmit }) => {
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!title && !content) {
-            alert('커뮤니티 이름과 개설 요청 이유를 입력해주세요.'); 
-        } else if (!title) {
-            alert('커뮤니티 이름을 입력해주세요.'); 
-        } else if (!content) {
-            alert('커뮤니티 개설 요청 이유를 입력해주세요.'); 
-        } else {
-            onSubmit({ title, content });
-            setTitle(''); 
-            setContent(''); 
-        }
-    };
-
-    const handleCancel = () => {
-        setTitle(''); // 제목 필드 초기화
-        setContent(''); // 내용 필드 초기화
-    };
-    
-    return (
-        <form onSubmit={handleSubmit}>
-            <h1>
-                <title>출품 상품 삭제 요청</title>
-            </h1>
-            <div>
-                <label htmlFor="title">커뮤니티 이름</label>
-                <input
-                    type="text"
-                    id="title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    placeholder="커뮤니티 이름을 입력해주세요 (30자)" 
-                    required
-                    maxLength={30} // 최대 글자 수 제한
-                />
-            </div>
-            <div>
-                <label htmlFor="content">개설 요청 이유</label>
-                <textarea
-                    id="content"
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                    placeholder="개설 요청 이유를 입력해주세요" 
-                    required
-                />
-            </div>
-            <button type="submit">확인</button>
-            <button type="button" onClick={handleCancel}>취소</button>
-        </form>
-    );
+  return (
+    <div className="post-form-container">
+      <h1> 커뮤니티 개설 요청 </h1>
+      <PostForm 
+        onSubmit={onSubmit} // PostForm에 onSubmit 전달
+        titlePlaceholder={titlePlaceholder} 
+        contentPlaceholder={contentPlaceholder}
+        titleLabel={titleLabel}
+        contentLabel={contentLabel}
+      />
+    </div>
+  );
 };
 
 export default RequestAddCommunity;
