0bf63bab1a6c8d20ddfee55724b86c1c9792e1ad
glennman1 <uuusop@naver.com>
2024-12-16 19:22:39 +0900

    test: onSubmit은 props->함수로 수정, RequestAddCommunity로 표기한 부분들 본래 파일 이름 및 관련된 부분들로 변동, h1태그 ->GodoTitleLabel 컴포넌트로 변동, 커뮤니티관련 내용들 -> 출품상품 삭제요청과 관련된 문구들로 변동 (#9)

--- fe/src/pages/RequestDeleteItem.jsx
+++ fe/src/pages/RequestDeleteItem.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import PostForm from '../components/PostForm'; // css 임포트
 
-const RequestAddCommunity = ({ onSubmit }) => {
+const RequestDelteItem = ({ ItemName }) => {
+  const onSubmit = () => {
   return (
     <div className="post-form-container">
-      <h1> 커뮤니티 개설 요청 </h1>
+      <GodoTitleLabel> 출품상품 삭제 요청 </GodoTitleLabel>
+      <h2>상품이름 : {ItemName}</h2>
       <PostForm 
         onSubmit={onSubmit} // PostForm에 onSubmit 전달
-        titlePlaceholder="커뮤니티 이름을 입력해주세요 (30자 이내)" 
-        contentPlaceholder="개설 요청 이유를 입력해주세요"
-        titleLabel="커뮤니티 이름"
-        contentLabel="개설 요청 이유"
+        titlePlaceholder="삭제 요청 제목을 입력해주세요 (30자 이내)" 
+        contentPlaceholder="삭제 요청 이유를 입력해주세요"
+        titleLabel="제목"
+        contentLabel="이유"
       />
     </div>
   );
 };
+};
 
-export default RequestAddCommunity;
+export default RequestDelteItem;
 
