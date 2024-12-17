2f9a9571cbd72d9e25058eef35b4b8678007882a
jooyeon <jooyeonstudyios@gmail.com>
2024-12-17 10:41:22 +0900

    design: 커뮤니티 정보 불러와서 커뮤니니티 이름  설정 (#16)

--- fe/src/pages/AuctionItemsPage.jsx
+++ fe/src/pages/AuctionItemsPage.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom'
+import { getCommunityInfo } from '../services/communityService';
+import GodoTitelLabe from '../components/Labels/GodoTitleLabel'
 
+// DT-01-02 출품 상품 목록 보기
 export default function AuctionItemsPage() {
+  const [communityInfo, setCommunityInfo] = useState('');
   const location = useLocation();
   const communityId = location.state.communityId;
+
+  const fetchCommunityInfo = async() => {
+    try {
+      const data = await getCommunityInfo(communityId);
+      setCommunityInfo(data);
+    } catch (error) {
+      console.error("Faild", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchCommunityInfo();
+  }, [])
   
   return (
     <>
-      <p>{communityId}</p>
+      <GodoTitelLabe text={communityInfo.name} />
     </>
   )
 }
--- fe/src/services/communityService.js
+++ fe/src/services/communityService.js
@@ -1,5 +1,24 @@
 // import axios from 'axios'
 
+// - 커뮤니티 정보 불러오기
+export async function getCommunityInfo(communityId) {
+  try {
+    // const response = await axios.get(`http://localhost:8080/api/communities/${communityId}`);
+    // return response.data;
+    const id = Number(communityId); // 백엔드 연결할 때는 딱히 필요 없을 것 같음 
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+      data.push({
+        name: `Community ${i}`,
+      });
+    }
+    return data[id];
+  } catch (error) {
+    console.error("Error fetching:", error);
+    throw error; 
+  }
+}
+
 // 홈 페이지 - 인기 커뮤니티
 export async function getPopularCommunities() {
   try {
