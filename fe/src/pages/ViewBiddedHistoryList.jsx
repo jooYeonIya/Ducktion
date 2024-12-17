7848a1eb1799899c02259ec34d6bb0263deed11d
jooyeon <jooyeonstudyios@gmail.com>
2024-12-17 19:45:03 +0900

    design: 정렬 옵션 추가 (#25)

--- fe/src/pages/ViewBiddedHistoryList.jsx
+++ fe/src/pages/ViewBiddedHistoryList.jsx
@@ -1,12 +1,35 @@
-import { useLocation } from "react-router-dom";
-import GodoTitleLabel from "../components/Labels/GodoTitleLabel";
+import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import GodoTitleLabel from '../components/Labels/GodoTitleLabel'
+import RoundButton from '../components/Button/RoundButton'
 
 export default function ViewBiddedHistoryList() {
+  const sortOptions = [
+    { value: "all", title: "전체" },
+    { value: "bidding", title: "입찰중" },
+    { value: "bidded", title: "낙찰" },
+    { value: "biddedCancel", title: "유찰" },
+  ];
+
+  const [sortOption, setSortOption] = useState(sortOptions[0].value);
+
   const location = useLocation();
-  
+
+  const handleSortChange = (value) => {
+    setSortOption(value);
+  }
+
   return (
     <>
-      <GodoTitleLabel text={"출력 이력"} />
+      <div className='biddedHistoryList_title'>
+        <GodoTitleLabel text={"출력 이력"} />
+      </div>
+
+      <div className='biddedHistoryLis_sortOption_container'>
+        <div className='biddedHistoryLis_sortOption'>
+          <RoundButton options={sortOptions} onChange={handleSortChange} />
+        </div>
+      </div>
     </>
   )
 }
