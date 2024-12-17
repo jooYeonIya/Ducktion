fa310288583de84f2977d3f3cace29e229eb4537
jooyeon <jooyeonstudyios@gmail.com>
2024-12-17 15:29:14 +0900

    design: 타이틀 3개 추가 (#20)

--- fe/src/pages/ViewSearchResult.jsx
+++ fe/src/pages/ViewSearchResult.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from 'react-router-dom'
+import GodoTitleLabel from '../components/Labels/GodoTitleLabel'
 
 export default function ViewSearchResult() {
   const location = useLocation();
@@ -6,7 +7,17 @@ export default function ViewSearchResult() {
 
   return (
     <>
-      <p>{searchText}</p>
+      <div className='viewSearchResult_title'>
+        <GodoTitleLabel text={"검색 결과"} />
+      </div>
+
+      <div className='viewSearchResult_community_container'>
+        <GodoTitleLabel text={"커뮤니티"} />
+      </div>
+
+      <div className='viewSearchResult_item_container'>
+        <GodoTitleLabel text={"출품 상품"} />
+      </div>
     </>
   )
 }
