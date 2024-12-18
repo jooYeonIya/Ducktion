733a292e87bbacd982b63373995639bb71a48579
glennman1 <uuusop@naver.com>
2024-12-18 17:32:20 +0900

    design: 내정보보기로 넘어갈 수 있게 버튼생성 및 기능구현 (#22)

---
+++ fe/src/pages/ViewMypage.jsx
@@ -0,0 +1,87 @@
+import { useNavigate } from "react-router-dom";
+import GodoTitleLabel from "../components/Labels/GodoTitleLabel";
+import PreCaptionLabel from "../components/Labels/PreCaptionLabel";
+import mockData from '../data/mockData.json';
+import '@styles/pages/ViewMypage.css'
+
+export default function ViewMypage() {
+  const biddedSortOptions = [
+    { value: "all", title: "전체" },
+    { value: "bidding", title: "입찰중" },
+    { value: "bidded", title: "낙찰" },
+    { value: "biddedCancel", title: "유찰" },
+  ];
+
+  const biddingSortOptions = [
+    { value: "all", title: "전체" },
+    { value: "bidding", title: "입찰중" },
+    { value: "bidded", title: "낙찰" },
+    { value: "biddedFail", title: "패찰" },
+  ];
+
+  const navigate = useNavigate();
+
+  const handleBiddedItem = () => {
+    navigate('/viewBiddedHistoryList', {state: {sortType: biddedSortOptions[1].value} })  
+  }
+
+  const handleBiddingItem = () => {
+    navigate('/viewBiddingHistoryList', {state: {sortType: biddingSortOptions[1].value} })  
+  }
+
+  const handleFavoriteItem = () => {
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+      data.push({
+        communityId: i,
+        image: "/src/assets/test_image.png",
+        favorited: i%2 == 0,
+        name: `${i} 상품`,
+        priceInfo: {price: i*10000, type: ""},
+        additionalInfo: null,
+        overlayText: false
+      });
+    }
+    navigate('/viewFavoriteItemList', { state: {items: data} })  
+  }
+
+  const nickname = mockData.nickname; // 변경
+  const phoneNumber = mockData.phoneNumber; // 변경
+  const address = mockData.address; // 변경
+
+  const moveViewMyInfo = () => {
+    navigate('/viewmyinfo', { state: { nickname, phoneNumber, address } }); //변경
+  };
+  
+  
+  return (
+    <>
+      <GodoTitleLabel text={"마이페이지"} />
+
+    <div className='moveViewMyInfo'>
+      <button onClick={moveViewMyInfo}>내 정보 보기</button>
+    </div>
+
+      <div className='biddingItem_container'>
+        <div className='biddingItem_container_title'>
+          <GodoTitleLabel text={"입찰 이력"} />
+          <button onClick={handleBiddingItem}><PreCaptionLabel text={"입찰 이력"} /></button>
+        </div>
+      </div>
+
+      <div className='biddedItem_container'>
+        <div className='biddedItem_container_title'>
+          <GodoTitleLabel text={"출품 이력"} />
+          <button onClick={handleBiddedItem}><PreCaptionLabel text={"출품 이력"} /></button>
+        </div>
+      </div>
+
+      <div className='favoriteItem_container'>
+        <div className='favoriteItem_container_title'>
+          <GodoTitleLabel text={"관심 상품"} />
+          <button onClick={handleFavoriteItem}><PreCaptionLabel text={"더보기"} /></button>
+        </div>
+      </div>
+    </>
+  )
+}
