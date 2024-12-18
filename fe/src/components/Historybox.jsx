7d2da5ca0ec309f980895b780b94f1f2ed7e1c36
jooyeon <jooyeonstudyios@gmail.com>
2024-12-18 21:10:31 +0900

    design: 화면 이동 링크 추가 (#34)

--- fe/src/components/Historybox.jsx
+++ fe/src/components/Historybox.jsx
@@ -3,12 +3,13 @@ import PreTitleLabel from './Labels/PreTitleLabel';
 
 import '@styles/components/Historybox.css'
 
-export default function Historybox({ items }) {
+export default function Historybox({ items, onClick }) {
   return (
     <div className='historybox'>
       {items.map((item, index) => (
         <div
           key={index}
+          onClick={() => onClick(item.value)}
           className={`historybox_item ${index !== items.length - 1 ? 'withBorder' : ''}`}
         >
           <PreTextLabel text={item.title} />
---
+++ fe/src/services/uesrService.js
@@ -0,0 +1,23 @@
+// import api from "./api";
+
+// 사용자 정보 불러오기
+export async function getUserInfo() {
+  try {
+    // const response = await api.get(`/user/info/${user_id}`);
+    // return response.data;
+    const data = {
+      email: "dlapdlf@adpad.com",
+      nickname: "bbbbbbbbsibaruaqewrgoi;huagderwhijklo;adfsclhkiuacdfse lhiku",
+      profileImage: "/src/assets/duck_selected.png",
+      phone: "00012345656",
+      address: "asgfhkladgslkhjiadfsgjkl;d",
+      heldBid: 100,
+      usableBid: 10934098,
+      rate: 12345
+    }
+    return data;
+  } catch (error) {
+    console.error("Error fetching:", error);
+    throw error;
+  }
+}
