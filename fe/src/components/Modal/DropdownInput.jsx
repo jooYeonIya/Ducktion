c80e01655d74c14ae4da3ba6505b58ed8289b54f
jooyeon <jooyeonstudyios@gmail.com>
2024-12-19 16:32:24 +0900

    design: 드롭다운 컴포넌트 추가 (#48)

---
+++ fe/src/components/Modal/DropdownInput.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "@styles/components/modal/DropdownInput.css";
+
+export default function DropdownInput({ dropList, placeholder, onSubmit }) {
+  const [selected, setSelected] = useState('');
+  const [inputText, setInputText] = useState('');
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleSelectedItem = (bank) => {
+    setSelected(bank);
+    setIsDropdownOpen(false);
+  };
+
+  const handleInputTextChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  return (
+    <div className="dropdownInput_container">
+      <div className="dropdownInput_select" onClick={() => setIsDropdownOpen((prev) => !prev)}>
+        {selected}
+      </div>
+
+      {isDropdownOpen && (
+        <div className="dropdownInput_selectDropdown">
+          {dropList.map((item, index) => (
+            <div
+              key={index}
+              className="dropdownInput_option"
+              onClick={() => handleSelectedItem(item)}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      )}
+
+      <input
+        type="text"
+        value={inputText}
+        onChange={handleInputTextChange}
+        className="dropdownInput_input"
+        placeholder={placeholder}
+      />
+    </div>
+  );
+}
