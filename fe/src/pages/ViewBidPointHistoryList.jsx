fee94737cc050fd46821e47e90c7a60fcd72fb3b
jooyeon <jooyeonstudyios@gmail.com>
2024-12-19 10:09:04 +0900

    design: 비드 이력 페이지 추가 및 화면 이동 (#44)

---
+++ fe/src/pages/ViewBidPointHistoryList.jsx
@@ -0,0 +1,9 @@
+import '@styles/pages/ViewBidPointHistoryList.css'
+
+export default function ViewBidPointHistoryList() {
+  return(
+    <>
+    adfl;kjegrwaqtio;jpreqwlijk;adfsckljadfsclkhj
+    </>
+  )
+}
