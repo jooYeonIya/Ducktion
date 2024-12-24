4b3a1c04372a301b94149e3d9773c6eb182fa70c
bbman9900 <tmddn1679@naver.com>
2024-12-24 17:37:49 +0900

    fix: 아이템 도메인 엔티티 수정 (#118)

---
+++ be/src/main/java/shop/duction/be/item/enums/RareTier.java
@@ -0,0 +1,14 @@
+package shop.duction.be.item.enums;
+
+public enum RareTier {
+    NORMAL, // 0 ~ 0.5
+    NORMAL_RARE,    // 0.5 ~ 1
+    RARE,   //  1 ~ 1.5
+    SUPER_RARE, // 1.5 ~ 2
+    ULTRA_RARE, // 2 ~ 2.5
+    ULTIMATE_RARE,  // 2.5 ~ 3
+    SECRET_RARE,    // 3 ~ 3.5
+    PARALLEL_RARE,  // 3.5 ~ 4
+    COLLECTORS_RARE,    // 4 ~ 4.5
+    MASTER_COLLECTORS_RARE  // 4.5 ~ 5
+}
