fbfdf56a3e4e7654da1cd4963553a0f2e32d5851
jooyeon <jooyeonstudyios@gmail.com>
2024-12-29 18:35:08 +0900

    design: 마감일 지났을 경우에는 입력 못 하도록 수정 (#107)

--- fe/src/components/Modal/InputInvoiceModalContent.jsx
+++ fe/src/components/Modal/InputInvoiceModalContent.jsx
@@ -12,6 +12,7 @@ export default function InputInvoiceModalContent({ itemId, onClose }) {
   const [selectedCourier, setSelectedCourier] = useState('');
   const [ivoice, setInvoice] = useState('');
   const [deadlineDate, setDeadlineDate] = useState('');
+  const [isDeadlineValid, setIsDeadlineValid] = useState(false);
 
   const handleCourierSelected = (value) => {
     setSelectedCourier(value);
@@ -25,6 +26,14 @@ export default function InputInvoiceModalContent({ itemId, onClose }) {
     try {
       const deadline = await getShippingDeadline(itemId);
       setDeadlineDate(deadline);
+
+      if (deadline === 'ADMIN') {
+        setIsDeadlineValid(true);
+      } else {
+        const deadlineDate = parseDateFromKoreanString(deadline);
+        const currentDate = new Date();
+        setIsDeadlineValid(currentDate <= deadlineDate);
+      }
     } catch (error) {
       console.error('Failed', error);
     }
@@ -55,21 +64,37 @@ export default function InputInvoiceModalContent({ itemId, onClose }) {
     }
   };
 
+  const parseDateFromKoreanString = (dateString) => {
+    // "2024년 12월 29일"에서 숫자 부분만 추출
+    const dateParts = dateString.match(/\d+/g); // ["2024", "12", "29"]
+    if (!dateParts || dateParts.length !== 3) {
+      console.error("Invalid Date Format: ", dateString);
+      return null;
+    }
+
+    const [year, month, day] = dateParts.map(Number);
+    return new Date(year, month - 1, day);
+  };
+
   useEffect(() => {
     fetchShippingDeadline();
-  }, []); 
+  }, []);
 
   return (
     <div className='modal_contaier'>
       <GodoTitleLabel text={"배송 번호 입력"} />
       <PreTextLabel text={`배송 번호 입력 마감일: ${deadlineDate}`} />
-      <DropdownInput
-        dropList={courier}
-        title={'택배사 선택'}
-        placeholder={'택배 번호 - 없이 입력'}
-        selectedList={handleCourierSelected}
-        inputed={handleInvoiceInputed}
-      />
+      {isDeadlineValid ? (
+        <DropdownInput
+          dropList={courier}
+          title={'택배사 선택'}
+          placeholder={'택배 번호 - 없이 입력'}
+          selectedList={handleCourierSelected}
+          inputed={handleInvoiceInputed}
+        />
+      ) : (
+        <PreTextLabel text="배송 번호 입력 기한이 지났습니다." style={{ color: 'red', textAlign: 'center' }} />
+      )}
 
       <div className='bidPointModalContent_buttons'>
         <RectangleButton text="취소" onClick={onClose} />
